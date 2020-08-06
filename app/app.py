b2795be0111633e75b3e66649f53138f6ccee202
Moishe Lettvin <moishel@gmail.com>
2020-08-05 20:08:30 -0600

    Fix app path.

---
+++ app/app.py
@@ -0,0 +1,23 @@
+import os
+
+from flask import Flask
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'obliquely-strategic.sqlite'),
+)
+
+app.config.from_pyfile('config.py', silent=True)
+
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
+
+# a simple page that says hello
+@app.route('/hello')
+def hello():
+    return 'Hello, World!'
+
